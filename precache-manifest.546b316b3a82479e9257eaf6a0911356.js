cd0a4d06a0cf10647007a80e356a660fc1851c31
11AN-CODE <you@example.com>
2025-02-03 13:06:36 +0530

    Updates

---
+++ precache-manifest.546b316b3a82479e9257eaf6a0911356.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5cf40beb260fa55d5754001527da6d3",
+    "url": "/Monzilla-TASk/index.html"
+  },
+  {
+    "revision": "72d2263ca5f127eac1c8",
+    "url": "/Monzilla-TASk/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "f06f41d9a96edc2525cf",
+    "url": "/Monzilla-TASk/static/js/2.3b9912cb.chunk.js"
+  },
+  {
+    "revision": "72d2263ca5f127eac1c8",
+    "url": "/Monzilla-TASk/static/js/main.23424865.chunk.js"
+  },
+  {
+    "revision": "d29b07cec4002ca240d5",
+    "url": "/Monzilla-TASk/static/js/runtime~main.e80c0be7.js"
+  }
+]);
